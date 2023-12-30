84525ff0e97364f5be6aadb203c7183d765a45e9
Josef Šimánek <josef.simanek@gmail.com>
2023-12-30 06:07:34 +0100

    Increment synced sequence.

--- scripts/sequences.sh
+++ scripts/sequences.sh
@@ -19,11 +19,12 @@ for seq in $SEQUENCES; do
         true
         # echo "[INFO] Sequence $seq is in sync with value $NEW_VAL."
     else
-        # Set the sequence value in the destination database
-        psql "$NEW_CONNECTION" -c "SELECT setval('$seq', $OLD_VAL, false);" &> /dev/null
-        # echo "[INFO] Synced sequence $seq. Old value: $NEW_VAL, New value: $OLD_VAL."
+        # Increment the OLD_VAL by 1
+        let "INCREMENTED_VAL=OLD_VAL+1"
+        # Set the sequence value in the destination database and increment it
+        psql "$NEW_CONNECTION" -c "SELECT setval('$seq', $INCREMENTED_VAL, false);" &> /dev/null
+        # echo "[INFO] Synced and incremented sequence $seq. Old value: $NEW_VAL, New value: $INCREMENTED_VAL."
     fi
 done
 
 echo "[INFO] Sequence sync check completed!"
-
